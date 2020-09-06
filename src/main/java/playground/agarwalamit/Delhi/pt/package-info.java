9b8ebd24cf09c1c07429df8adb2ea3358a00ffb8
amit2011 <vandu_123>
2020-09-06 16:08:54 +0530

    updaing package file

--- src/main/java/playground/agarwalamit/Delhi/pt/package-info.java
+++ src/main/java/playground/agarwalamit/Delhi/pt/package-info.java
@@ -3,15 +3,17 @@ package playground.agarwalamit.Delhi.pt;
 /**
  * <li>GTFS files for Delhi bus lines are available from https://opendata.iiitd.edu.in/data/static/.</li>
  * <li>GTFS2MATSimSchedule class from  https://github.com/amit2011/GTFS2MATSim.git is used to convert GTFS to MATSim transit schedule.</li>
- * <li>The GTFS from OpenTransitData does not include metro lines, which are extracted using OSM. For this, Osm2TransitSchedule class from https://github.com/matsim-org/pt2matsim.git is used to generate MATSim Transit Scheudles.
- * <li>The class is throwing an error because of same transit line ID which is derived from the relation (ref/name/operator/ etc.).</li>
- * <li>To fix this, if a line ID exists already, another line ID is created in an incremental order. Ideally, these will be transit routes of a transit line.</li>
- * <li>The bus line info appears to be wrong. Remove them.</li>
- * <li>The output MATSim transit scheudle file for metro lines still lack in many lines.</li>
+ * <li>The GTFS from OpenTransitData does not include metro lines, which are extracted using OSM. For this, Osm2TransitSchedule class from https://github.com/matsim-org/pt2matsim.git is used to generate MATSim Transit Schedules.
+ * <li>The OSM file for Metro lines ONLY is generated using https://overpass-turbo.eu/ and the used script is shown at: https://github.com/amit2011/resources</li>
+ * <li>However, the OSM file is fixed as follows:
+ * <li>The key 'operaor' is made uniform everywhere.</li>
+ * <li>public final static String STATION = "station"; has been added to Osm class. Without it, lines were created only if "public_trnasport" tag has the key "stop_position".</li>
+ * <li>The tag public_transport was missing from a few nodes (around 25 stations). This is added as "<tag k="public_transport" v="stop_position"/>" in osm file.</li>
+ * <li> Operator name was added to grey line; ref is fixed for grey and airport express lines.</li>
  * </li>
- * <li>The generated GTFS is cleaned to extract only DMRC lines in it. See {@link playground.agarwalamit.Delhi.pt.OSMTransitSchedulePrep}</li>
  * <li>Next steps are:
  * <li>Merge the two MATSIm transit schedules.</li>
- * <li>Add the missing transit lines.</li>
+ * <li>Add the departure times</li>
+ * <li>The linkref IDs are missing from stop facilities which appears to be the reason for missing connections in VIA.</li>
  * </li>
  */
