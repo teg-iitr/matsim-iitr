aa76e2286adc0684bd252080befd02efd7615ac7
Nidhi510 <nidhikathaitar@gmail.com>
2020-09-18 11:41:18 +0530

    adding plans code

---
+++ src/main/java/playground/kathaitnidhi/grid/Population.java
@@ -0,0 +1,13 @@
+package playground.kathaitnidhi.grid;
+
+public class Population {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String netExample = "C:\\Users\\Nidhi\\Documents\\Matsim\\Grid\\Network.xml";
+		String plans ="C:\\Users\\Nidhi\\Documents\\Matsim\\Grid\\Plans.xml";
+
+	}
+
+}
