127baf09cffab8ef7c12d90ca1d99c0548281f27
Amit Agarwal <amitfce@iitr.ac.in>
2020-09-19 12:40:27 +0530

    removed copied classes by NK

--- src/main/java/playground/amit/gridNet/GridDRTExample.java
+++ src/main/java/playground/amit/gridNet/GridDRTExample.java
@@ -1,7 +1,7 @@
 package playground.amit.gridNet;
 
-import org.matsim.contrib.drt.run.RunDrtScenario;
-import org.matsim.contrib.edrt.run.RunEDrtScenario;
+import org.matsim.contrib.drt.run.DrtControlerCreator;
+import playground.amit.utils.NumberUtils;
 
 /**
  * Created by Amit on 18/09/2020.
@@ -10,6 +10,10 @@ public class GridDRTExample {
 
     String netExample = "C:\\Users\\Amit\\Google Drive\\iitr_gmail_drive\\project_data\\matsim_grid\\gridNet.xml.gz";
     String configFile = "";
+
+    DrtControlerCreator controlerCreator = new DrtControlerCreator();
+
+    GridNetwork gridNetwork = new GridNetwork();
 
 
 
