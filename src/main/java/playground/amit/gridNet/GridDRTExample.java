7589bf3a4f43af6c39f3410e13604f7158424b18
amit2011 <vandu_123>
2020-09-20 20:29:14 +0530

    more fixes to run DRT example

--- src/main/java/playground/amit/gridNet/GridDRTExample.java
+++ src/main/java/playground/amit/gridNet/GridDRTExample.java
@@ -1,8 +1,5 @@
 package playground.amit.gridNet;
 
-import org.matsim.contrib.drt.run.DrtControlerCreator;
-import playground.amit.utils.NumberUtils;
-
 /**
  * Created by Amit on 18/09/2020.
  */
@@ -10,8 +7,6 @@ public class GridDRTExample {
 
     String netExample = "C:\\Users\\Amit\\Google Drive\\iitr_gmail_drive\\project_data\\matsim_grid\\gridNet.xml.gz";
     String configFile = "";
-
-    DrtControlerCreator controlerCreator = new DrtControlerCreator();
 
     GridNetwork gridNetwork = new GridNetwork();
     
--- src/test/java/playground/amit/DRTExample.java
+++ src/test/java/playground/amit/DRTExample.java
@@ -1,17 +1,37 @@
 package playground.amit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.drt.run.examples.RunDrtExample;
 import org.matsim.contrib.drt.run.examples.RunOneSharedTaxiExample;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
+import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.vis.otfvis.OTFVisConfigGroup;
 
 import java.net.URL;
 
 public class DRTExample {
 
+    @Rule
+    public MatsimTestUtils utils = new MatsimTestUtils();
+
     @Test
     public void testRunDrtExample() {
         URL configUrl = IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("dvrp-grid"), "one_shared_taxi_config.xml");
-        RunOneSharedTaxiExample.run(configUrl, false, 1);
+        RunOneSharedTaxiExample.run(configUrl, true, 1);
+
+//        URL configUrl = IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("mielec"), "mielec_stop_based_drt_config.xml");
+//        Config config = ConfigUtils.loadConfig(configUrl, new MultiModeDrtConfigGroup(), new DvrpConfigGroup(),
+//                new OTFVisConfigGroup());
+//
+//        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+//        config.controler().setOutputDirectory(utils.getOutputDirectory());
+//        RunDrtExample.run(config, true);
     }
 }
