04f6d82b4257bc12e7ee3e7228e682f05d0a882c
Nidhi510 <nidhikathaitar@gmail.com>
2020-12-31 12:39:32 +0530

    minor changes

--- src/main/java/playground/amit/Delhi/MalviyaNagarPT/analysis/MNmodeShareAnalysis.java
+++ src/main/java/playground/amit/Delhi/MalviyaNagarPT/analysis/MNmodeShareAnalysis.java
@@ -1,8 +1,14 @@
 package playground.amit.Delhi.MalviyaNagarPT.analysis;
 
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
+
+import java.util.List;
+import java.util.Map;
 
 public class MNmodeShareAnalysis {
     public static void main(String[] args) {
@@ -17,8 +23,25 @@ public class MNmodeShareAnalysis {
         reader.readFile(inputFile);
         events.finishProcessing();
 
-//        System.out.println(mnModeShareHandler.modeShare());
+
+        System.out.println("Total number of person + driver= " + mnModeShareHandler.getPerson_MainMode().size());
+//        System.out.println(mnModeShareHandler.getPerson_MainMode());
+        int cntPt= mnModeShareHandler.getCntOfPtTrips();
+        int cntWalk=mnModeShareHandler.getCntOfWalkTrips();
+        System.out.println("Count of Pt trips= " + cntPt + ", Count of Walk trips= "+ cntWalk);
+
+
+        int totalTrips=cntPt+cntWalk;
+        double ptShare = (double) 100 * cntPt/totalTrips;
+        double walkShare =(double) 100 * cntWalk/totalTrips;
+        System.out.println("Share of Pt trips= " + ptShare + ", Share of Walk trips= "+ walkShare);
+
+
 //
-//        System.out.println("Events file read!");
+
+
     }
+
+
+
 }
