30f756a0f269d76b83a3fb1d9a6a135a7b1caffb
Madhu Patel <54492585+Madhupatel08@users.noreply.github.com>
2023-03-30 15:48:34 +0530

    Update EventHandlerLanes.java

--- src/main/java/playground/shivam/signals/EventHandlerLanes.java
+++ src/main/java/playground/shivam/signals/EventHandlerLanes.java
@@ -16,46 +16,65 @@ import org.matsim.lanes.Lane;
 import org.matsim.lanes.LanesToLinkAssignment;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class EventHandlerLanes implements LaneEnterEventHandler, LaneLeaveEventHandler {
     private HashMap<String, Double> entryTime = new HashMap<>();
     private HashMap<String, Double>exitTime = new HashMap<>();
-    private HashMap<String, Integer> numVehicles = new HashMap<>();
+    private static Config config = ConfigUtils.loadConfig("output/RunFixedMixedTrafficSignalSimpleIntersection/config.xml");
+    private static Scenario scenario = ScenarioUtils.loadScenario(config);
+    private static Network network;
+
+    private static HashMap<String, Integer> numVehicles = new HashMap<>();
+    private static HashSet<String>allLanes = new HashSet<>();
+
     @Override
     public void handleEvent(LaneEnterEvent event) {
-        String laneId = event.getLaneId().toString();
+        config.network().getLaneDefinitionsFile();
+        network = scenario.getNetwork();
 
-        if(laneId.equals("2_3.l") || laneId.equals("2_3.r") || laneId.equals("2_3.ol") || laneId.equals("2_3.s") ||
-                laneId.equals("4_3.l") || laneId.equals("4_3.r") || laneId.equals("4_3.s") ||  laneId.equals("2_3.ol")
-                || laneId.equals("7_3.l") || laneId.equals("7_3.r") || laneId.equals("7_3.s") ||  laneId.equals("7_3.ol")
-                || laneId.equals("8_3.l") || laneId.equals("8_3.r") || laneId.equals("8_3.s") || laneId.equals("8_3.ol")){
-            entryTime.put(laneId, event.getTime());
+        for (LanesToLinkAssignment l2l : scenario.getLanes().getLanesToLinkAssignments().values()){
+            for (Lane lane : l2l.getLanes().values()) {
+                String laneId = lane.getId().toString();
+                allLanes.add(laneId);
+            }
+        }
+        String currentLaneId = event.getLaneId().toString();
+
+        for (String lanes : allLanes) {
+            if (currentLaneId.equals(lanes)) {
+                double travelTime = entryTime.getOrDefault(currentLaneId, 0.0);
+                entryTime.put(currentLaneId, travelTime + event.getTime());
+            }
         }
     }
 
     @Override
     public void handleEvent(LaneLeaveEvent event) {
-        String laneId = event.getLaneId().toString();
-        if(laneId.equals("2_3.l") || laneId.equals("2_3.r") || laneId.equals("2_3.ol") || laneId.equals("2_3.s") ||
-                laneId.equals("4_3.l") || laneId.equals("4_3.r") || laneId.equals("4_3.s") ||  laneId.equals("2_3.ol")
-              || laneId.equals("7_3.l") || laneId.equals("7_3.r") || laneId.equals("7_3.s") ||  laneId.equals("7_3.ol")
-              || laneId.equals("8_3.l") || laneId.equals("8_3.r") || laneId.equals("8_3.s") || laneId.equals("8_3.ol") ){
-//            System.out.println("LaneId: " + event.getLaneId() + "  LaneLeaveEvent  " + "Time  " + event.getTime());
-            double travelTime = exitTime.getOrDefault(laneId, 0.0);
-            exitTime.put(laneId, travelTime+event.getTime());
+        config.network().getLaneDefinitionsFile();
+        network = scenario.getNetwork();
 
-            int count = numVehicles.getOrDefault(laneId, 0);
-            numVehicles.put(laneId, count + 1);
+        for (LanesToLinkAssignment l2l : scenario.getLanes().getLanesToLinkAssignments().values()){
+            for (Lane lane : l2l.getLanes().values()) {
+                String laneId = lane.getId().toString();
+                allLanes.add(laneId);
+            }
+        }
+        String currentLaneId = event.getLaneId().toString();
+        for (String lanes : allLanes) {
+            if (currentLaneId.equals(lanes)) {
+                double travelTime = exitTime.getOrDefault(currentLaneId, 0.0);
+                exitTime.put(currentLaneId, travelTime + event.getTime());
+
+                int count = numVehicles.getOrDefault(currentLaneId, 0);
+                numVehicles.put(currentLaneId, count + 1);
+            }
         }
     }
+
     public double getTravelTimeLanes(String laneId){
-        if(entryTime.get(laneId) == null && exitTime.get(laneId) == null){
-            return 0.0;
-        }
         double totalTravelTime = Math.abs(exitTime.get(laneId) - entryTime.get(laneId));
         int totalNumVehicles = numVehicles.get(laneId);
-//        System.out.println("Inside lanes Event Handler" + totalTravelTime);
-
         return totalTravelTime / totalNumVehicles;
     }
 }
